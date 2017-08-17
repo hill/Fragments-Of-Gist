16b35ea9bf1ced9e7b0f46316efbfbc2b79b55c0
Tom Hill <tomhill98@me.com>
2017-08-18 06:46:41 +1000

    Finish completed!

--- assets/js/page-specific/camera.js
+++ assets/js/page-specific/camera.js
@@ -1,20 +1,66 @@
 const howler = require('howler');
 const Mousetrap = require('mousetrap');
+const $ = require('jquery');
+const ScrollMagic = require('scrollmagic');
+require('scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators');
+$(document).ready(function ($) {
 
 navigator.webkitGetUserMedia({video: true},
   function(stream) {
     document.getElementById('camera').src = URL.createObjectURL(stream);
   },
   function() {
-    alert('could not connect stream');
+    alert("You don't have a camera! :( ");
   }
 );
 
-new Howl({
+
+
+var controller = new ScrollMagic.Controller();
+
+var shower = new Howl({
+    src: ['../assets/sounds/shower.mp3'],
+    volume: 0.8,
+    loop: true,
+})
+
+shower.play()
+
+
+var playing = false
+// Audio
+var psycho = new Howl({
     src: ['../assets/sounds/psycho-theme.mp3'],
     volume: 1.5,
     loop: true,
-}).play();
+    onplay: function() {
+      playing = true
+    },
+    onend: function() {
+      console.log('Finished hitchcock!');
+      playing = false
+    }
+});
+
+new ScrollMagic.Scene({
+  triggerElement: '#psy'
+})
+.on('enter', function(){
+  console.log('Entered psycho');
+  shower.fade(0.5, 0, 1000);
+  psycho.volume(1);
+  psycho.play();
+})
+.on("leave", function(ev){
+  console.log('Left Meaning')
+  shower.fade(0,0.8,1000);
+  //chorale.stop();
+  psycho.fade(1, 0, 1000);
+})
+//.addIndicators()
+.addTo(controller)
+
+});
 
 // Shortcuts (Not DRY but I don't give a shit...this is due in 1 day)
 
